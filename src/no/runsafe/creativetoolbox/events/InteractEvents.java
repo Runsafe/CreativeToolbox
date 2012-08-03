99ea4175ea3b4955e6e1e9089cf178d71594a69f
Morten Nilsen <morten@runsafe.no>
2012-08-03 23:07:44 +0200

    Only check owned regions when holding configured item!

--- src/no/runsafe/creativetoolbox/events/InteractEvents.java
+++ src/no/runsafe/creativetoolbox/events/InteractEvents.java
@@ -9,7 +9,6 @@ import no.runsafe.framework.server.event.player.RunsafePlayerInteractEntityEvent
 import no.runsafe.framework.server.event.player.RunsafePlayerInteractEvent;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.worldguardbridge.WorldGuardInterface;
-import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 
 import java.util.List;
@@ -17,93 +16,89 @@ import java.util.Set;
 
 public class InteractEvents implements IPlayerInteractEvent, IPlayerInteractEntityEvent
 {
-    public InteractEvents(IConfiguration configuration)
-    {
-        this.configuration = configuration;
-        this.worldGuardInterface = RunsafePlugin.Instances.get("RunsafeWorldGuardBridge").getComponent(WorldGuardInterface.class);
-    }
+	public InteractEvents(IConfiguration configuration)
+	{
+		this.configuration = configuration;
+		this.worldGuardInterface = RunsafePlugin.Instances.get("RunsafeWorldGuardBridge").getComponent(WorldGuardInterface.class);
+	}
 
-    @Override
-    public void OnPlayerInteractEvent(RunsafePlayerInteractEvent event)
-    {
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK)
-        {
-            try
-            {
-                if (event.getItemStack().getTypeId() == this.configuration.getConfigValueAsInt("list_item"))
-            {
-                this.listPlotsByLocation(new RunsafeLocation(event.getBlock().getLocation()), event.getPlayer());
-                event.setCancelled(true);
-            }
-            }
-            catch (NullPointerException e)
-            {
-                // Player punched floor with air, ignore this. Also, Pete sucks.
-            }
-        }
-    }
+	@Override
+	public void OnPlayerInteractEvent(RunsafePlayerInteractEvent event)
+	{
+		if (event.getAction() == Action.RIGHT_CLICK_BLOCK)
+		{
+			if (event.getItemStack() != null && event.getItemStack().getTypeId() == this.configuration.getConfigValueAsInt("list_item"))
+			{
+				this.listPlotsByLocation(new RunsafeLocation(event.getBlock().getLocation()), event.getPlayer());
+				event.setCancelled(true);
+			}
+		}
+	}
 
-    @Override
-    public void OnPlayerInteractEntityEvent(RunsafePlayerInteractEntityEvent event)
-    {
-        if (event.getRightClicked() instanceof RunsafePlayer)
-        {
-            this.listPlotsByPlayer((RunsafePlayer) event.getRightClicked(), event.getPlayer());
-            event.setCancelled(true);
-        }
-    }
+	@Override
+	public void OnPlayerInteractEntityEvent(RunsafePlayerInteractEntityEvent event)
+	{
+		if (event.getRightClicked() instanceof RunsafePlayer)
+		{
+			if (event.getPlayer().getItemInHand().getItemId() == this.configuration.getConfigValueAsInt("list_item"))
+			{
+				this.listPlotsByPlayer((RunsafePlayer) event.getRightClicked(), event.getPlayer());
+				event.setCancelled(true);
+			}
+		}
+	}
 
-    private void listPlotsByPlayer(RunsafePlayer checkPlayer, RunsafePlayer triggerPlayer)
-    {
-        if (!this.worldGuardInterface.serverHasWorldGuard())
-        {
-            triggerPlayer.sendMessage("Error: No WorldGuard installed.");
-            return;
-        }
+	private void listPlotsByPlayer(RunsafePlayer checkPlayer, RunsafePlayer triggerPlayer)
+	{
+		if (!this.worldGuardInterface.serverHasWorldGuard())
+		{
+			triggerPlayer.sendMessage("Error: No WorldGuard installed.");
+			return;
+		}
 
-        List<String> regions = worldGuardInterface.getOwnedRegions(checkPlayer, checkPlayer.getWorld());
+		List<String> regions = worldGuardInterface.getOwnedRegions(checkPlayer, checkPlayer.getWorld());
 
-        if (!regions.isEmpty())
-            for (String regionName : regions)
-                this.listRegion(regionName, triggerPlayer, true);
-        else
-            triggerPlayer.sendMessage("No regions owned by this player.");
-    }
+		if (!regions.isEmpty())
+			for (String regionName : regions)
+				this.listRegion(regionName, triggerPlayer, true);
+		else
+			triggerPlayer.sendMessage("No regions owned by this player.");
+	}
 
-    private void listPlotsByLocation(RunsafeLocation location, RunsafePlayer player)
-    {
-        if (!this.worldGuardInterface.serverHasWorldGuard())
-        {
-            player.sendMessage("Error: No WorldGuard installed.");
-            return;
-        }
+	private void listPlotsByLocation(RunsafeLocation location, RunsafePlayer player)
+	{
+		if (!this.worldGuardInterface.serverHasWorldGuard())
+		{
+			player.sendMessage("Error: No WorldGuard installed.");
+			return;
+		}
 
-        List<String> regions = worldGuardInterface.getRegionsAtLocation(location);
+		List<String> regions = worldGuardInterface.getRegionsAtLocation(location);
 
-        if (!regions.isEmpty())
-            for (String regionName : regions)
-                this.listRegion(regionName, player, false);
-        else
-            player.sendMessage("No regions found at this point.");
-    }
+		if (!regions.isEmpty())
+			for (String regionName : regions)
+				this.listRegion(regionName, player, false);
+		else
+			player.sendMessage("No regions found at this point.");
+	}
 
-    private void listRegion(String regionName, RunsafePlayer player, Boolean simple)
-    {
-        player.sendMessage("Region: " + regionName);
+	private void listRegion(String regionName, RunsafePlayer player, Boolean simple)
+	{
+		player.sendMessage("Region: " + regionName);
 
-        if (!simple)
-        {
-            Set<String> owners = worldGuardInterface.getOwners(player.getWorld(), regionName);
-            Set<String> members= worldGuardInterface.getMembers(player.getWorld(), regionName);
+		if (!simple)
+		{
+			Set<String> owners = worldGuardInterface.getOwners(player.getWorld(), regionName);
+			Set<String> members = worldGuardInterface.getMembers(player.getWorld(), regionName);
 
-            for (String owner : owners)
-                player.sendMessage("     Owner: " + owner);
+			for (String owner : owners)
+				player.sendMessage("     Owner: " + owner);
 
-            for (String member : members)
-                player.sendMessage("     Member: " + member);
-        }
-    }
+			for (String member : members)
+				player.sendMessage("     Member: " + member);
+		}
+	}
 
-    private IConfiguration configuration;
-    private WorldGuardInterface worldGuardInterface;
+	private IConfiguration configuration;
+	private WorldGuardInterface worldGuardInterface;
 }
