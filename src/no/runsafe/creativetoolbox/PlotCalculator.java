c28d57e76724969c8169743be6eec9adbd52d28b
Morten Nilsen <morten@runsafe.no>
2013-01-05 19:13:37 +0100

    Collect plot calculations into a new class

---
+++ src/no/runsafe/creativetoolbox/PlotCalculator.java
@@ -0,0 +1,12 @@
+package no.runsafe.creativetoolbox;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mortenn
+ * Date: 05.01.13
+ * Time: 16:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlotCalculator
+{
+}
