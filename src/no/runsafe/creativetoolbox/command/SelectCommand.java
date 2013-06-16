11f79be98037607c82a39b92fdcea1a9a35d548d
Morten Nilsen <morten@runsafe.no>
2013-06-16 15:18:16 +0200

    Support multiplots for /ct sel

--- src/no/runsafe/creativetoolbox/command/SelectCommand.java
+++ src/no/runsafe/creativetoolbox/command/SelectCommand.java
@@ -5,9 +5,11 @@ import no.runsafe.framework.RunsafePlugin;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.minecraft.player.RunsafePlayer;
 import no.runsafe.worldeditbridge.WorldEditInterface;
+import no.runsafe.worldguardbridge.WorldGuardInterface;
 
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
+import java.util.List;
 
 public class SelectCommand extends PlayerCommand
 {
@@ -15,13 +17,19 @@ public class SelectCommand extends PlayerCommand
 	{
 		super("select", "Sets your WorldEdit region to the plot you are in", "runsafe.creative.select");
 		worldEdit = RunsafePlugin.getFirstPluginAPI(WorldEditInterface.class);
+		worldGuard = RunsafePlugin.getFirstPluginAPI(WorldGuardInterface.class);
 		plotCalculator = calculator;
 	}
 
 	@Override
 	public String OnExecute(RunsafePlayer executor, HashMap<String, String> parameters)
 	{
-		Rectangle2D area = plotCalculator.getPlotArea(executor.getLocation());
+		List<String> candidate = worldGuard.getRegionsAtLocation(executor.getLocation());
+		Rectangle2D area;
+		if (candidate != null && candidate.size() == 1)
+			area = worldGuard.getRectangle(executor.getWorld(), candidate.get(0));
+		else
+			area = plotCalculator.getPlotArea(executor.getLocation());
 		worldEdit.select(
 			executor,
 			plotCalculator.getMinPosition(executor.getWorld(), area),
@@ -30,6 +38,7 @@ public class SelectCommand extends PlayerCommand
 		return null;
 	}
 
-	private WorldEditInterface worldEdit;
+	private final WorldEditInterface worldEdit;
+	private final WorldGuardInterface worldGuard;
 	private final PlotCalculator plotCalculator;
 }
