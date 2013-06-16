2179d38f8ef78a3136680dbc55b69494cea0b318
Morten Nilsen <morten@runsafe.no>
2013-06-16 23:40:32 +0200

    Add a repository for player votes

---
+++ src/no/runsafe/creativetoolbox/database/PlotVoteRepository.java
@@ -0,0 +1,52 @@
+package no.runsafe.creativetoolbox.database;
+
+import no.runsafe.framework.api.database.IDatabase;
+import no.runsafe.framework.api.database.Repository;
+import no.runsafe.framework.minecraft.player.RunsafePlayer;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PlotVoteRepository extends Repository
+{
+	public PlotVoteRepository(IDatabase database)
+	{
+		this.database = database;
+	}
+
+	public boolean recordVote(RunsafePlayer player, String plot)
+	{
+		return database.Update(
+			"INSERT INTO creative_plot_vote (`plot`, `player`, `rank`) VALUES (?, ?, ?)" +
+				"ON DUPLICATE KEY UPDATE rank=VALUES(`rank`)",
+			plot, player.getName(), StringUtils.join(player.getGroups(), ",")
+		) > 0;
+	}
+
+	@Override
+	public String getTableName()
+	{
+		return "creative_plot_vote";
+	}
+
+	@Override
+	public HashMap<Integer, List<String>> getSchemaUpdateQueries()
+	{
+		HashMap<Integer, List<String>> revisions = new HashMap<Integer, List<String>>();
+		List<String> sql = new ArrayList<String>();
+		sql.add(
+			"CREATE TABLE creative_plot_vote (" +
+				"`plot` varchar(255) NOT NULL," +
+				"`player` varchar(255) NOT NULL," +
+				"`rank` varchar(255) NOT NULL," +
+				"PRIMARY KEY(`plot`,`player`)" +
+				")"
+		);
+		revisions.put(1, sql);
+		return revisions;
+	}
+
+	private final IDatabase database;
+}
