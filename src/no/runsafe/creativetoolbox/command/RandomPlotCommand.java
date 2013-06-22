5329025f96f17ae5e91e8448a1e4c82ca91bf908
Morten Nilsen <morten@runsafe.no>
2013-06-22 20:20:44 +0200

    Support an optional tag to look for in /ct randomplot

--- src/no/runsafe/creativetoolbox/command/RandomPlotCommand.java
+++ src/no/runsafe/creativetoolbox/command/RandomPlotCommand.java
@@ -1,6 +1,7 @@
 package no.runsafe.creativetoolbox.command;
 
 import no.runsafe.creativetoolbox.PlotFilter;
+import no.runsafe.creativetoolbox.database.PlotTagRepository;
 import no.runsafe.framework.api.IScheduler;
 import no.runsafe.framework.api.command.player.PlayerAsyncCallbackCommand;
 import no.runsafe.framework.minecraft.player.RunsafePlayer;
@@ -11,19 +12,37 @@ import java.util.Random;
 
 public class RandomPlotCommand extends PlayerAsyncCallbackCommand<RandomPlotCommand.Sudo>
 {
-	public RandomPlotCommand(PlotFilter filter, IScheduler scheduler)
+	public RandomPlotCommand(PlotFilter filter, IScheduler scheduler, PlotTagRepository tagRepository)
 	{
 		super("randomplot", "teleport to a random plot.", "runsafe.creative.teleport.random", scheduler);
 		plotFilter = filter;
+		this.tagRepository = tagRepository;
 		rng = new Random();
 	}
 
 	@Override
 	public Sudo OnAsyncExecute(RunsafePlayer executor, HashMap<String, String> parameters)
 	{
+		return null;
+	}
+
+	@Override
+	public Sudo OnAsyncExecute(RunsafePlayer executor, HashMap<String, String> parameters, String[] arguments)
+	{
 		if (plotFilter.getWorld() == null)
 			return null;
-		List<String> plots = plotFilter.getFiltered();
+		List<String> plots;
+		if (arguments.length > 0)
+		{
+			plots = tagRepository.findPlots(arguments[0]);
+			if (plots.isEmpty())
+			{
+				executor.sendColouredMessage("&cSorry, found no plots tagged \"%s\".", arguments[0]);
+				return null;
+			}
+		}
+		else
+			plots = plotFilter.getFiltered();
 		int r = rng.nextInt(plots.size());
 		Sudo target = new Sudo();
 		target.player = executor;
@@ -44,6 +63,7 @@ public class RandomPlotCommand extends PlayerAsyncCallbackCommand<RandomPlotComm
 		public String command;
 	}
 
+	private final PlotTagRepository tagRepository;
 	private final PlotFilter plotFilter;
 	private final Random rng;
 }
