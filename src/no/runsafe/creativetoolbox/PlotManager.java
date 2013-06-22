eb2868293d0a59391e3e979d03581c596d699a15
Morten Nilsen <morten@runsafe.no>
2013-06-22 19:27:02 +0200

    Tune formatting a touch more

--- src/no/runsafe/creativetoolbox/PlotManager.java
+++ src/no/runsafe/creativetoolbox/PlotManager.java
@@ -239,13 +239,13 @@ public class PlotManager implements IConfigurationChanged, IPluginEnabled, IPlay
 		{
 			PlotApproval approved = plotApproval.get(plot);
 			if (approved != null && approved.getApproved() != null)
-				tags.add(String.format("&2[&aapproved &2%s]&r", dateFormat.print(approved.getApproved())));
+				tags.add(String.format("&2[&aapproved &a%s&2]&r", dateFormat.print(approved.getApproved())));
 		}
 		if (player.hasPermission("runsafe.creative.vote.tally"))
 		{
 			int voteCount = voteRepository.tally(plot);
 			if (voteCount > 0)
-				tags.add(String.format("&7[&a%d&2 vote%s&7]&r", voteCount, voteCount > 1 ? "s" : ""));
+				tags.add(String.format("&2[&a%d&2 vote%s]&r", voteCount, voteCount > 1 ? "s" : ""));
 		}
 		return Strings.join(tags, " ");
 	}
