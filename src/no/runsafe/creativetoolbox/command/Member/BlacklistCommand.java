46a3329030e4a8752a8467c12e0a46a20abddb9a
Morten Nilsen <morten@runsafe.no>
2013-07-13 14:52:14 +0200

    Do not lock up the main thread when blacklisting someone

--- src/no/runsafe/creativetoolbox/command/Member/BlacklistCommand.java
+++ src/no/runsafe/creativetoolbox/command/Member/BlacklistCommand.java
@@ -2,7 +2,8 @@ package no.runsafe.creativetoolbox.command.Member;
 
 import no.runsafe.creativetoolbox.PlotManager;
 import no.runsafe.creativetoolbox.database.PlotMemberBlacklistRepository;
-import no.runsafe.framework.api.command.ExecutableCommand;
+import no.runsafe.framework.api.IScheduler;
+import no.runsafe.framework.api.command.AsyncCommand;
 import no.runsafe.framework.api.command.ICommandExecutor;
 import no.runsafe.framework.minecraft.RunsafeServer;
 import no.runsafe.framework.minecraft.player.RunsafeAmbiguousPlayer;
@@ -10,17 +11,17 @@ import no.runsafe.framework.minecraft.player.RunsafePlayer;
 
 import java.util.HashMap;
 
-public class BlacklistCommand extends ExecutableCommand
+public class BlacklistCommand extends AsyncCommand
 {
-	public BlacklistCommand(PlotMemberBlacklistRepository blacklistRepository, PlotManager manager)
+	public BlacklistCommand(PlotMemberBlacklistRepository blacklistRepository, PlotManager manager, IScheduler scheduler)
 	{
-		super("blacklist", "Blocks a certain player from being added to any additional creative plots", "runsafe.creative.blacklist", "player");
+		super("blacklist", "Blocks a certain player from being added to any additional creative plots", "runsafe.creative.blacklist", scheduler, "player");
 		this.blacklistRepository = blacklistRepository;
 		this.manager = manager;
 	}
 
 	@Override
-	protected String OnExecute(ICommandExecutor executor, HashMap<String, String> params)
+	public String OnAsyncExecute(ICommandExecutor executor, HashMap<String, String> params)
 	{
 		RunsafePlayer player = RunsafeServer.Instance.getPlayer(params.get("player"));
 		if (player == null)
@@ -36,6 +37,7 @@ public class BlacklistCommand extends ExecutableCommand
 		manager.removeMember(player);
 		return String.format("The player %s has been blacklisted.", player.getPrettyName());
 	}
+
 
 	private final PlotMemberBlacklistRepository blacklistRepository;
 	private final PlotManager manager;
