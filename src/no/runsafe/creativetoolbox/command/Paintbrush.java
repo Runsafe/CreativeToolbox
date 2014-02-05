9866f4aeda1e3392d00f5fcc0f3822da5cc650ad
Kruithne <kruithne@gmail.com>
2014-02-05 23:33:46 +0000

    More debugging on the class name we're getting!

--- src/no/runsafe/creativetoolbox/command/Paintbrush.java
+++ src/no/runsafe/creativetoolbox/command/Paintbrush.java
@@ -1,11 +1,11 @@
 package no.runsafe.creativetoolbox.command;
 
-import net.minecraft.server.v1_7_R1.NBTTagByte;
-import net.minecraft.server.v1_7_R1.NBTTagCompound;
-import net.minecraft.server.v1_7_R1.NBTTagList;
+import net.minecraft.server.v1_7_R1.ItemStack;
 import no.runsafe.framework.api.command.argument.IArgumentList;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.internal.wrapper.ObjectUnwrapper;
+import no.runsafe.framework.minecraft.Enchant;
 import no.runsafe.framework.minecraft.Item;
 import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
 
@@ -22,7 +22,14 @@ public class Paintbrush extends PlayerCommand
 		RunsafeMeta brush = Item.Materials.BlazeRod.getItem();
 		brush.setDisplayName("Paintbrush");
 
-		NBTTagCompound tag = brush.getTagCompound();
+		ItemStack raw = ObjectUnwrapper.getMinecraft(brush);
+		if (raw == null)
+			executor.sendColouredMessage("It's null..");
+		else
+			executor.sendColouredMessage(raw.getClass().getName());
+
+
+		/*NBTTagCompound tag = brush.getTagCompound();
 
 		if (tag == null)
 			tag = new NBTTagCompound();
@@ -33,7 +40,7 @@ public class Paintbrush extends PlayerCommand
 
 		NBTTagCompound newTag = brush.getTagCompound();
 		if (newTag == null)
-			executor.sendColouredMessage("It's null..");
+			executor.sendColouredMessage("It's null..");*/
 
 		executor.give(brush);
 		return "&aConjured a paintbrush! Left click to select a paint block, right click to paint with it!";
