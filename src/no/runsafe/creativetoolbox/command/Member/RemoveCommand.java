36225bf792c27db62fc948e264f07df37c9cdbe1
njs41 <njs42@hotmail.com>
2017-06-01 17:28:35 -0400

    Make the remove member command more like the add member command.

--- src/no/runsafe/creativetoolbox/command/Member/RemoveCommand.java
+++ src/no/runsafe/creativetoolbox/command/Member/RemoveCommand.java
@@ -4,7 +4,6 @@ import no.runsafe.creativetoolbox.PlotFilter;
 import no.runsafe.creativetoolbox.database.PlotMemberRepository;
 import no.runsafe.creativetoolbox.event.PlotMembershipRevokedEvent;
 import no.runsafe.framework.api.IScheduler;
-import no.runsafe.framework.api.IServer;
 import no.runsafe.framework.api.command.argument.IArgumentList;
 import no.runsafe.framework.api.command.argument.Player;
 import no.runsafe.framework.api.command.player.PlayerAsyncCommand;
@@ -14,48 +13,42 @@ import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class RemoveCommand extends PlayerAsyncCommand
 {
-	public RemoveCommand(IScheduler scheduler, PlotFilter filter, IRegionControl worldGuard, PlotMemberRepository memberRepository, IServer server)
+	public RemoveCommand(IScheduler scheduler, PlotFilter filter, IRegionControl worldGuard, PlotMemberRepository memberRepository)
 	{
 		super("remove", "Remove a member from the plot you are standing in.", "runsafe.creative.member.remove", scheduler, new Player().require());
 		plotFilter = filter;
 		worldGuardInterface = worldGuard;
 		this.memberRepository = memberRepository;
-		this.server = server;
 	}
 
 	@Override
 	public String OnAsyncExecute(IPlayer executor, IArgumentList parameters)
 	{
-		List<String> targets = plotFilter.apply(worldGuardInterface.getRegionsAtLocation(executor.getLocation()));
-		if (targets == null || targets.isEmpty())
-			return "No region defined at your location!";
+		IPlayer member = parameters.getValue("player");
 
+		if (member == null)
+			return null;
+
+		List<String> targetPlots = plotFilter.apply(worldGuardInterface.getRegionsAtLocation(executor.getLocation()));
 		List<String> ownedRegions = worldGuardInterface.getOwnedRegions(executor, plotFilter.getWorld());
-		List<String> results = new ArrayList<String>();
-		String filter = ((IPlayer) parameters.getValue("player")).getName().toLowerCase();
-		for (String region : targets)
+		if (targetPlots == null || targetPlots.isEmpty())
+			return "No region defined at your location!";
+		List<String> results = new ArrayList<>();
+		for (String region : targetPlots)
 		{
 			if (ownedRegions.contains(region) || executor.hasPermission("runsafe.creative.member.override"))
 			{
-				Set<String> members = worldGuardInterface.getMembers(executor.getWorld(), region);
-				for (String member : members)
-					if (member.toLowerCase().startsWith(filter))
-					{
-						IPlayer target = server.getOfflinePlayerExact(member);
-						assert (target != null);
-						if (worldGuardInterface.removeMemberFromRegion(plotFilter.getWorld(), region, target))
-						{
-							memberRepository.removeMember(region, member);
-							results.add(String.format("%s was successfully removed from the plot %s.", target.getPrettyName(), region));
-							new PlotMembershipRevokedEvent(target, region).Fire();
-						}
-						else
-							results.add(String.format("Could not remove %s from the plot %s.", target.getPrettyName(), region));
-					}
+				if (worldGuardInterface.removeMemberFromRegion(plotFilter.getWorld(), region, member))
+				{
+					memberRepository.removeMember(region, member.getName());
+					results.add(String.format("%s was successfully removed from the plot %s.", member.getPrettyName(), region));
+					new PlotMembershipRevokedEvent(member, region).Fire();
+				}
+				else
+					results.add(String.format("Could not remove %s from the plot %s.", member.getPrettyName(), region));
 			}
 			else
 				results.add(String.format("You do not appear to be an owner of %s.", region));
@@ -68,5 +61,4 @@ public class RemoveCommand extends PlayerAsyncCommand
 	private final IRegionControl worldGuardInterface;
 	private final PlotFilter plotFilter;
 	private final PlotMemberRepository memberRepository;
-	private final IServer server;
 }
